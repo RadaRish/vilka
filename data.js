8f0420b383f7161d89e4beada6b88e12bc087648
RadaRish <107124341+RadaRish@users.noreply.github.com>
2023-10-01 09:50:00 +0300

    3d tour Vilka Pskov

---
+++ data.js
@@ -0,0 +1,367 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "ул. Свердлова",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.2515900882926907,
+        "pitch": 0.02026555947416675,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.388260710821676,
+          "pitch": 0.18843348607510535,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.090382196703814,
+          "pitch": 0.2825348891276853,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": 0.011279765963832844,
+          "pitch": 0.1882736458242693,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Магазин Вилка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.10471975511965326,
+        "pitch": -0.022798754408437816,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1393040769070186,
+          "pitch": 0.4045954971545296,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.5732054419842108,
+          "pitch": 0.16411154927799565,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.8516625720734297,
+          "pitch": 0.13568869128260275,
+          "rotation": 0,
+          "target": "4---"
+        },
+        {
+          "yaw": 0.642307247539895,
+          "pitch": 0.18757365489719824,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": 1.1841891474191044,
+          "pitch": 0.0913264980997468,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Сиропы",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.984616146303642,
+          "pitch": 0.24959917955652955,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 1.6757020307482833,
+          "pitch": 0.2297867846032986,
+          "rotation": 0,
+          "target": "4---"
+        },
+        {
+          "yaw": 2.7859236144847372,
+          "pitch": 0.12936674425451855,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": 3.0276755242760656,
+          "pitch": 0.09700072135993665,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "Посуда и упаковка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.172018107893482,
+          "pitch": 0.25832263750351103,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.2277068254533843,
+          "pitch": 0.17766394456584322,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 1.519294766984845,
+          "pitch": 0.13255123068872265,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Расходные материалы",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6565201510584,
+          "pitch": 0.2804916574017522,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -2.0556520146729547,
+          "pitch": 0.12129429757117904,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.718778261065376,
+          "pitch": 0.13737724271268803,
+          "rotation": 0,
+          "target": "4---"
+        },
+        {
+          "yaw": 1.309036929306945,
+          "pitch": 0.2084476253776728,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "Упаковка, химия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1592583693984864,
+          "pitch": 0.5593465258629671,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": -2.880022456755249,
+          "pitch": 0.16811938823435923,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.5934575565189597,
+          "pitch": 0.1695394975217841,
+          "rotation": 0,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
